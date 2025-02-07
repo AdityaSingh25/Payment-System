72102b9ed243fd7633b1df82d6ca171cbc460278
AdityaSingh25 <richiaditya25@gmail.com>
2025-02-08 01:18:20 +0530

    tailwind started

---
+++ Tailwind/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  // return (
+  //   <div className="flex justify-between">
+  //     <div className="bg-red-900">Hi</div>
+  //     <div className="bg-green-200">Hi</div>
+  //     <div className="bg-yellow-500">Hi</div>
+  //     <div className="bg-white-400">Hi</div>
+  //   </div>
+  // );
+
+  // return (
+  //   <>
+  //     <div className="grid grid-cols-10">
+  //       <div className="bg-red-200 col-span-4">Hi there from first div</div>
+  //       <div className="bg-yellow-300 col-span-4">Hi there from second div</div>
+  //       <div className="bg-pink-200 col-span-2">Hi there from third div</div>
+  //     </div>
+  //     <div className="flex">
+  //       <div className="bg-red-200 w-[40%]">Hi there from first div</div>
+  //       <div className="bg-yellow-300 w-[40%]">Hi there from second div</div>
+  //       <div className="bg-pink-200 w-[20%]">Hi there from third div</div>
+  //     </div>
+  //   </>
+
+  return (
+    <>
+      <div className="bg-red-500 md:bg-blue-500">Hi there</div>
+
+      {/*by default it will be red & if width goes beyond md then it will change to blue*/}
+    </>
+  );
+}
+
+export default App;
+
+// sm- 640px
+// md- 768px
+// lg- 1024px
+// xl- 1280px
+// 2xl- 1536px
