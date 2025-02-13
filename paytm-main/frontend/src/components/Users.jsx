5f9ba66e8b71928099c7c8eb6fc654c13098ad27
AdityaSingh25 <richiaditya25@gmail.com>
2025-02-14 02:55:51 +0530

    filtering users in useEffect done

--- paytm-main/frontend/src/components/Users.jsx
+++ paytm-main/frontend/src/components/Users.jsx
@@ -1,21 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "./Button";
+import axios from "axios";
 
 export default function Users() {
   // Replace with backend call
-  const [users, setUsers] = useState([
-    {
-      firstName: "Aditya",
-      lastName: "Singh",
-      _id: 1,
-    },
-  ]);
+  const [users, setUsers] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    axios
+      .get("http://localhost:3000/api/v1/user/bulk?filter=" + filter, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        setUsers(response.data.users);
+      });
+  }, [filter]);
 
   return (
     <>
       <div className="font-bold mt-6 text-lg">Users</div>
       <div className="my-2">
         <input
+          onChange={(e) => {
+            setFilter(e.target.value);
+          }}
           type="text"
           placeholder="Search users..."
           className="w-full px-2 py-1 border rounded border-slate-200"
